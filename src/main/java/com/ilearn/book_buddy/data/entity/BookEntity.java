585226725be94cf0dea3e5a0498d1e7bf4cd49f5
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-09 10:34:45 +0100

    add book entity

---
+++ src/main/java/com/ilearn/book_buddy/data/entity/BookEntity.java
@@ -0,0 +1,40 @@
+package com.ilearn.book_buddy.data.entity;
+
+import com.ilearn.book_buddy.data.enums.BookType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+@Entity
+@Getter
+@Setter
+public class BookEntity extends AppendableReferenceEntity {
+    @Column(nullable = false, length = 30)
+    private String name;
+    @Column(nullable = false, unique = true)
+    @Min(value = 1, message = "Please enter an isbn")
+    private int isbn;
+    @Column(nullable = false)
+    private LocalDate publishDate;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    private BookType bookType;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
+    private String createdBy;
+
+
+}
