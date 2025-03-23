136c6e9bc74f0fe2308baeff44510faf248dcda7
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-23 17:27:16 +0100

    add book controller

--- src/main/java/com/ilearn/book_buddy/controller/BookController.java
+++ src/main/java/com/ilearn/book_buddy/controller/BookController.java
@@ -1,12 +1,15 @@
 package com.ilearn.book_buddy.controller;
 
+import com.ilearn.book_buddy.constants.UrlConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.ilearn.book_buddy.constants.UrlConstants.URL_CONSTANT;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/book")
+@RequestMapping(URL_CONSTANT )
 public class BookController {
 
 }
