5f161c6c59b32cfc1a1450d4a6b344055039caa4
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-24 21:43:42 +0100

    add book repo

--- src/main/java/com/ilearn/book_buddy/repository/BookRepository.java
+++ src/main/java/com/ilearn/book_buddy/repository/BookRepository.java
@@ -1,13 +1,13 @@
 package com.ilearn.book_buddy.repository;
 
-import com.ilearn.book_buddy.data.entity.BookEntity;
+import com.ilearn.book_buddy.data.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface BookRepository extends JpaRepository<BookEntity, String>, JpaSpecificationExecutor<BookEntity> {
-    Optional<BookEntity> findBookByIsbn(int isbn);
+public interface BookRepository extends JpaRepository<Book, String>, JpaSpecificationExecutor<Book> {
+    Optional<Book> findBookByIsbn(int isbn);
 
     boolean existsByIsbn(int isbn);
 
