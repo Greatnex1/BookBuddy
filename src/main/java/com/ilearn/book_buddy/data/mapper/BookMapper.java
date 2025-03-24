a1b43ef4e235d3b62337e1bc8bb524b4f24773cd
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-24 21:43:22 +0100

    add mapper

--- src/main/java/com/ilearn/book_buddy/data/mapper/BookMapper.java
+++ src/main/java/com/ilearn/book_buddy/data/mapper/BookMapper.java
@@ -1,10 +1,9 @@
 package com.ilearn.book_buddy.data.mapper;
 
-import com.ilearn.book_buddy.data.entity.BookEntity;
-import com.ilearn.book_buddy.data.model.Book;
+import com.ilearn.book_buddy.data.entity.Book;
 import com.ilearn.book_buddy.rest.request.BookRequest;
 
 public interface BookMapper {
-    BookEntity toBookEntity(BookRequest book);
-    BookRequest toBookObject(BookEntity bookEntity);
+    Book toBookEntity(BookRequest book);
+    BookRequest toBookObject(Book book);
 }
