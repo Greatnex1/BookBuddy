5da7d66f82ed85bf67f152cdb191c900d490dc70
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-26 11:04:45 +0100

    add book response

---
+++ src/main/java/com/ilearn/book_buddy/rest/response/BookResponse.java
@@ -0,0 +1,22 @@
+package com.ilearn.book_buddy.rest.response;
+
+import com.ilearn.book_buddy.data.enums.BookType;
+import com.ilearn.book_buddy.data.enums.Gender;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class BookResponse {
+    private String title;
+    private String description;
+    private int isbn;
+    private int publicationYear;
+    private long price;
+    private LocalDate dateUploaded;
+    private String reference;
+    private String url;
+}
