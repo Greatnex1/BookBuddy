e7aebfe9980c1f327502b0e8dba7c8f3cb6f3c9b
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-30 21:03:05 +0100

    deleted

--- src/main/java/com/ilearn/book_buddy/data/mapper/BookMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ilearn.book_buddy.data.mapper;
-
-import com.ilearn.book_buddy.data.entity.Book;
-import com.ilearn.book_buddy.rest.request.BookRequest;
-
-public interface BookMapper {
-    Book toBookEntity(BookRequest book);
-    BookRequest toBookObject(Book book);
-}
