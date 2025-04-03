f4fcd1f34d0f90f865e86f913a256e673e2c6af8
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-03 14:22:54 +0100

    update user entity

--- src/main/java/com/ilearn/book_buddy/data/entity/ApplicationUserEntity.java
+++ src/main/java/com/ilearn/book_buddy/data/entity/ApplicationUserEntity.java
@@ -1,11 +1,20 @@
 package com.ilearn.book_buddy.data.entity;
 
 import com.ilearn.book_buddy.data.enums.Gender;
+import com.ilearn.book_buddy.data.enums.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @NoArgsConstructor
@@ -14,7 +23,7 @@ import java.io.Serializable;
 @Setter
 @Entity
 @Table(name = "user_entities")
-public class ApplicationUserEntity extends AppendableReferenceEntity{
+public class ApplicationUserEntity extends AppendableReferenceEntity implements UserDetails {
 
     private String firstName;
 
@@ -31,4 +40,43 @@ public class ApplicationUserEntity extends AppendableReferenceEntity{
     private boolean emailVerified;
 
     private boolean enabled;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
