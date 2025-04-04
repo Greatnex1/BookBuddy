35cddf8bd424a82e8f1fb03f4681099201283ced
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-04 12:08:55 +0100

    add security

--- src/main/java/com/ilearn/book_buddy/config/UnauthorizedEntryPoint.java
+++ src/main/java/com/ilearn/book_buddy/config/UnauthorizedEntryPoint.java
@@ -11,10 +11,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.time.Instant;
 @Slf4j
+@Component
 public class UnauthorizedEntryPoint implements AuthenticationEntryPoint {
 
     @Override
