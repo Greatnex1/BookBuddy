b1b138dc42aee33e213dfc208b16fbd39028c4e3
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-04 12:30:59 +0100

    add register request

---
+++ src/main/java/com/ilearn/book_buddy/rest/request/RegisterRequest.java
@@ -0,0 +1,27 @@
+package com.ilearn.book_buddy.rest.request;
+
+import com.ilearn.book_buddy.data.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "firstname is required")
+    private String firstName;
+    @NotBlank(message = "lastname is required")
+    private String lastName;
+    @NotBlank(message = "email is required")
+    @Email(message = "email format is not valid")
+    private String email;
+    @NotBlank(message = "password is required")
+//    @StrongPassword
+    private String password;
+    @NotNull
+    private Role role;
+}
