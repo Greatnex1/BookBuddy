038bcf4ac346a91e9f77c625bf68ee804352cf95
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-07 17:04:39 +0100

    add jwt use-case

--- src/main/java/com/ilearn/book_buddy/service/interfaces/JwtUseCase.java
+++ src/main/java/com/ilearn/book_buddy/service/interfaces/JwtUseCase.java
@@ -1,5 +1,14 @@
 package com.ilearn.book_buddy.service.interfaces;
 
-public interface JwtUseCase {
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.userdetails.UserDetails;
 
+public interface JwtUseCase {
+    String extractUserName(String token);
+    String generateToken(UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
+    ResponseCookie generateJwtCookie(String jwt);
+    String getJwtFromCookies(HttpServletRequest request);
+    ResponseCookie getCleanJwtCookie();
 }
