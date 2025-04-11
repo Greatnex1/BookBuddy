4532029987a1a8248dd72a2a108da34b1ad74141
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-11 20:10:11 +0100

    refactor author use_case

--- src/main/java/com/ilearn/book_buddy/service/interfaces/AuthorUseCase.java
+++ src/main/java/com/ilearn/book_buddy/service/interfaces/AuthorUseCase.java
@@ -1,14 +1,16 @@
 package com.ilearn.book_buddy.service.interfaces;
 
 import com.ilearn.book_buddy.data.entity.Author;
+import com.ilearn.book_buddy.rest.request.AuthorRequest;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 
 public interface AuthorUseCase {
     Author createAuthor(AuthorRequest author);
-    void updateAuthor(long id, AuthorRequest updateAuthorRequest) throws InvocationTargetException, IllegalAccessException;
-    void deleteAuthor(long id);
+    void save(Author author);
+    void updateAuthor(String id, AuthorRequest updateAuthorRequest) throws InvocationTargetException, IllegalAccessException;
+    void deleteAuthor(String id);
     Optional<Author> findByEmail(String email);
-    Optional<Author> findById(long id);
+    Optional<Author> findById(String id);
 }
