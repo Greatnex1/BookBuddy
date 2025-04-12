fd101d44970088b23bca24bdd74fe74b8b275e77
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-12 14:58:34 +0100

    refactored app user repository

--- src/main/java/com/ilearn/book_buddy/repository/ApplicationUserRepository.java
+++ src/main/java/com/ilearn/book_buddy/repository/ApplicationUserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ApplicationUserRepository extends JpaRepository<ApplicationUserEntity, String> {
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUserEntity, Long> {
     Optional<ApplicationUserEntity> findByEmail(String email);
 
 }
