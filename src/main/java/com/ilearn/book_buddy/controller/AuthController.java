7ebd031edc55ab881fcd4def2f0d4587a5c530aa
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-12 14:57:07 +0100

    add authentication api

---
+++ src/main/java/com/ilearn/book_buddy/controller/AuthController.java
@@ -0,0 +1,116 @@
+package com.ilearn.book_buddy.controller;
+
+import com.ilearn.book_buddy.rest.request.AuthenticationRequest;
+import com.ilearn.book_buddy.rest.request.RefreshTokenRequest;
+import com.ilearn.book_buddy.rest.request.RegisterRequest;
+import com.ilearn.book_buddy.rest.response.AuthenticationResponse;
+import com.ilearn.book_buddy.rest.response.RefreshTokenResponse;
+import com.ilearn.book_buddy.service.implementation.AuthenticationService;
+import com.ilearn.book_buddy.service.interfaces.JwtUseCase;
+import com.ilearn.book_buddy.service.interfaces.RefreshTokenUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.*;
+
+import static com.ilearn.book_buddy.constants.UrlConstants.URL;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(URL+ "/auth")
+@SecurityRequirements()
+//@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+@Tag(name= "Authentication Endpoints")
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+    private final RefreshTokenUseCase refreshTokenService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUseCase jwtService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
+        AuthenticationResponse authenticationResponse = authenticationService.register(request);
+        ResponseCookie jwtCookie = jwtService.generateJwtCookie(authenticationResponse.getAccessToken());
+        ResponseCookie refreshTokenCookie = refreshTokenService.generateRefreshTokenCookie(authenticationResponse.getRefreshToken());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .header(HttpHeaders.SET_COOKIE,refreshTokenCookie.toString())
+                .body(authenticationResponse);
+    }
+
+    @PostMapping("/login")
+    @Operation(
+            responses = {
+                    @ApiResponse(
+                            description = "Login successful",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Authorization is required",
+                            responseCode = "401",
+                            content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")}
+                    )
+            }
+    )
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        AuthenticationResponse authenticationResponse = authenticationService.authenticate(request);
+        ResponseCookie jwtCookie = jwtService.generateJwtCookie(authenticationResponse.getAccessToken());
+        ResponseCookie refreshTokenCookie = refreshTokenService.generateRefreshTokenCookie(authenticationResponse.getRefreshToken());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE,jwtCookie.toString())
+                .header(HttpHeaders.SET_COOKIE,refreshTokenCookie.toString())
+                .body(authenticationResponse);
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<RefreshTokenResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
+        return ResponseEntity.ok(refreshTokenService.generateNewToken(request));
+    }
+
+    @PostMapping("/refresh-token-cookie")
+    public ResponseEntity<Void> refreshTokenCookie(HttpServletRequest request) {
+        String refreshToken = refreshTokenService.getRefreshTokenFromCookies(request);
+        RefreshTokenResponse refreshTokenResponse = refreshTokenService
+                .generateNewToken(new RefreshTokenRequest(refreshToken));
+        ResponseCookie NewJwtCookie = jwtService.generateJwtCookie(refreshTokenResponse.getAccessToken());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, NewJwtCookie.toString())
+                .build();
+    }
+    @GetMapping("/info")
+    public Authentication getAuthentication(@RequestBody AuthenticationRequest request){
+        return  authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request){
+        String refreshToken = refreshTokenService.getRefreshTokenFromCookies(request);
+        if(refreshToken != null) {
+            refreshTokenService.deleteByToken(refreshToken);
+        }
+        ResponseCookie jwtCookie = jwtService.getCleanJwtCookie();
+        ResponseCookie refreshTokenCookie = refreshTokenService.getCleanRefreshTokenCookie();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE,jwtCookie.toString())
+                .header(HttpHeaders.SET_COOKIE,refreshTokenCookie.toString())
+                .build();
+
+    }
+
+}
