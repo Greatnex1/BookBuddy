37ff674f6b98554d0f4f139847997b32a11f6449
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-27 05:58:28 +0100

    add const messages

---
+++ src/main/java/com/ilearn/book_buddy/constants/ErrorMessages.java
@@ -0,0 +1,36 @@
+package com.ilearn.book_buddy.constants;
+
+public class ErrorMessages {
+    public static final String NOT_FOUND = "Not Found";
+    public static final String ALREADY_EXIST = "Already exist";
+    public static final String INVALID_EMAIL = "Invalid Email";
+    public static final String EMPTY_INPUT_ERROR = "Input not valid";
+    public static final String UNDEFINED = "Undefined";
+
+    public final static String APPENDABLE_SEPARATOR = "_";
+    public final static String ID = "id";
+    public static final String PRICE_CANT_BE_NULL= "Price cannot be null";
+    public final static String REFERENCE = "reference";
+    public static final String QUERY_PROP_SEPARATOR = ".";
+    public static final String GREATER_THAN_INCLUSIVE = ":>";
+    public static final String EQUAL = ":";
+    public static final String NOT_EQUAL = "-";
+    public static final String GREATER_THAN = ">";
+    public static final String LESSER_THAN_INCLUSIVE = ":<";
+    public static final String LESSER_THAN = "<";
+    public static final String LIKE = "~";
+    public static final String OR_OPERATOR = "|";
+    public static final String FULLTEXT = "fulltext";
+    public static final String BOOK_NOT_FOUND = "Book not found";
+    public static final String CART_NOT_FOUND = "Shopping cart not found";
+    public static final String INSUFFICIENT_BALANCE = "Insufficient Balance";
+    public static final String INVALID_ACCOUNT_NUMBER = "Invalid Account Number";
+    public static final String USER_ALREADY_EXIST = "User already exist";
+    public static final String CART_IS_EMPTY = "Shopping Cart is Empty";
+    public static final String INVALID_GENRE = "Invalid Genre";
+    public static final String INVALID_ISBN = "ISBN must contain only numbers and dash(-)";
+    public static final String INVALID_TITLE = "Title must contain only numbers and letters";
+    public static final String INVALID_PAYMENT = "Only USSD, Transfer and Internet Banking is available";
+    public static final String INVALID_TRANSACTION_PIN = "Invalid transaction pin";
+    public static final String OUT_OF_STOCK = "Out of stock";
+}
