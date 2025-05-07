3d65375a71ad0e3d212149d07ad785e24601ca52
Greatnex1 <noahgreatakoni@gmail.com>
2025-05-07 10:23:58 +0100

    add input validator

---
+++ src/main/java/com/ilearn/book_buddy/validator/InputValidator.java
@@ -0,0 +1,28 @@
+package com.ilearn.book_buddy.validator;
+
+import com.ilearn.book_buddy.constants.ErrorMessages;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+
+import static com.ilearn.book_buddy.constants.ErrorMessages.EMPTY_INPUT_ERROR;
+
+public class InputValidator {
+
+    public static void validateInput(String input, String data) {
+        if(StringUtils.isEmpty(input)|| StringUtils.isBlank(input)|| StringUtils.isEmpty(input.trim())||
+                input.equals(ErrorMessages.UNDEFINED)){
+            throw new IllegalArgumentException(String.format(EMPTY_INPUT_ERROR, data));
+
+        }
+    }
+    
+    public static void validateInput(String input) {
+        if(StringUtils.isEmpty(input)|| StringUtils.isBlank(input)|| StringUtils.isEmpty(input.trim())||
+                input.equals(ErrorMessages.UNDEFINED)){
+
+            throw new IllegalArgumentException(ErrorMessages.EMPTY_INPUT_ERROR);
+
+        }
+    }
+}
