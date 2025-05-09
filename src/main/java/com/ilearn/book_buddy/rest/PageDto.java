3f2ba9f4dfd0483fed3c4b37d4cb8fc0c1640ce4
Greatnex1 <noahgreatakoni@gmail.com>
2025-05-09 15:20:36 +0100

    add page dto

---
+++ src/main/java/com/ilearn/book_buddy/rest/PageDto.java
@@ -0,0 +1,31 @@
+package com.ilearn.book_buddy.rest;
+
+import com.ilearn.book_buddy.data.enums.ResponseStatus;
+import lombok.*;
+import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PageDto {
+    private List<?> data;
+    private boolean hasMore;
+    private int currentPage;
+    private int totalPages;
+    private long totalItems;
+    private ResponseStatus status;
+
+    public static PageDto empty() {
+        return PageDto.builder().data(new ArrayList<>()).totalPages(0).totalItems(0)
+                .status(ResponseStatus.SUCCESS).build();
+    }
+
+   
+}
